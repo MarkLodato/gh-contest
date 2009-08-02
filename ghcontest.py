97769921a75e65c3c40f801ac5a5ab3aa7894074
Mark Lodato <lodatom@gmail.com>
2009-08-02 12:36:11 -0400

    first real attempt

---
+++ ghcontest.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import itertools
+
+def read_data(filename):
+    with open(filename) as f:
+        pairs = [map(int, line.rstrip('\n\r').split(':')) for line in f]
+    num_users = max(x[0] for x in pairs) + 1
+    num_repos = max(x[1] for x in pairs) + 1
+    u2r = [[] for i in range(num_users)]
+    r2u = [[] for i in range(num_repos)]
+    for u,r in pairs:
+        u2r[u].append(r)
+        r2u[r].append(u)
+    return u2r, r2u
+
+def read_test(filename):
+    with open(filename) as f:
+        return [int(x) for x in f]
+
+def count_sum(iterable):
+    d = {}
+    for k,v in iterable:
+        try:
+            d[k] += v
+        except KeyError:
+            d[k] = v
+    return d
+
+def count(iterable):
+    return count_sum(itertools.izip(iterable, itertools.repeat(1)))
+
+def suggest(user, u2r, r2u, n=None, cutoff=None):
+    repos = u2r[user]
+
+    other_users = count_sum( (u,1.0)
+            for r in repos
+            for u in r2u[r])
+    other_users.pop(user, None)
+
+    if cutoff is not None:
+        other_users = other_users.items()
+        other_users.sort(reverse=True, key=lambda x: x[1])
+        other_users = dict(other_users[:cutoff])
+
+    other_repos = count_sum( (r,v)
+            for u,v in other_users.iteritems()
+            for r in u2r[u] )
+    for r in repos:
+        other_repos.pop(r, None)
+
+    results = other_repos.items()
+    results.sort(reverse=True, key=lambda x: x[1])
+
+    if n is None:
+        return results
+    else:
+        return [x[0] for x in results[:n]]
+
+def run_suggest(users, u2r, r2u, n=10):
+    return dict( (u, suggest(u, u2r, r2u, n)) for u in users )
+
+def save_suggest(filename, results):
+    with open(filename, 'w') as f:
+        for k,v in sorted(results.iteritems()):
+            f.write('%s:%s\n' % (k, ','.join(map(str,v))))
