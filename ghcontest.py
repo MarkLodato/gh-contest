04c7be2cd0d9c3c8168638f02508f39783958dbc
Mark Lodato <lodatom@gmail.com>
2009-08-28 22:07:19 -0400

    add code for best result

--- ghcontest.py
+++ ghcontest.py
@@ -33,91 +33,241 @@ __metaclass__ = type
 #   THE SOFTWARE.
 
 import itertools
+import sys
+import re
+import scipy.sparse
+import numpy as np
 
-def read_data(filename):
-    with open(filename) as f:
-        pairs = [map(int, line.rstrip('\n\r').split(':')) for line in f]
-    num_users = max(x[0] for x in pairs) + 1
-    num_repos = max(x[1] for x in pairs) + 1
-    u2r = [[] for i in range(num_users)]
-    r2u = [[] for i in range(num_repos)]
-    for u,r in pairs:
-        u2r[u].append(r)
-        r2u[r].append(u)
-    return u2r, r2u
+class Suggestion:
 
-def read_test(filename):
-    with open(filename) as f:
-        return [int(x) for x in f]
+    data_filename = 'download/data.txt'
+    followers_filename = 'download/followers.txt'
+    lang_filename = 'download/lang.txt'
+    repos_filename = 'download/repos.txt'
 
-def read_repos(filename):
-    r2from = {}
-    with open(filename) as f:
-        for line in f:
-            r,v = line.rstrip('\n\r').split(':',1)
-            v = v.split(',')
-            if len(v) == 3:
-                name, date, forked_from = v
-                r2from[int(r)] = int(forked_from)
-    return r2from
+    def __init__(self):
+        self.data = self.load_data()
+        self.followers = self.load_followers()
+        self.forked = self.load_forked()
 
-def count_sum(iterable):
-    d = {}
-    for k,v in iterable:
+    def load_data(self):
+        filename = self.data_filename
+        return self.sparse_from_text(filename)
+
+    def load_followers(self):
+        filename = self.followers_filename
         try:
-            d[k] += v
-        except KeyError:
-            d[k] = v
-    return d
+            n = self.data.shape[0]
+            shape = (n,n)
+        except AttributeError:
+            shape = None
+        return self.sparse_from_text(filename, shape=shape)
 
-def count(iterable):
-    return count_sum(itertools.izip(iterable, itertools.repeat(1)))
+    def load_forked(self):
+        filename = self.repos_filename
+        try:
+            n = self.data.shape[1]
+            shape = (n,n)
+        except AttributeError:
+            shape = None
+        return self.forked_from_text(filename, shape=shape)
 
-def suggest(user, u2r, r2u, r2from, n=None, cutoff=None):
-    repos = u2r[user]
-    all_repos = list(repos)
+    @staticmethod
+    def sparse_from_text(filename, shape=None):
+        data = np.loadtxt(filename, delimiter=':', dtype=int)
+        values = np.ones((len(data),), dtype=float)
+        ij = np.transpose(data)
+        return scipy.sparse.csr_matrix((values,ij), shape=shape)
 
-    if r2from is not None:
-        for r in repos:
-            try:
-                f = r2from[r]
-            except KeyError:
-                pass
+    @staticmethod
+    def forked_from_text(filename, shape=None):
+        r = re.compile(r'^(\d+):.*,.*,(\d+)$')
+        r2from = []
+        with open(filename) as f:
+            for line in f:
+                m = r.match(line)
+                if m is not None:
+                    r2from.append([int(x) for x in m.groups()])
+        data = np.array(r2from)
+        values = np.ones((len(data),), dtype=float)
+        ij = np.transpose(data)
+        return scipy.sparse.csr_matrix((values,ij), shape=shape)
+
+    @staticmethod
+    def multiply_broadcast(A, m):
+        """Multiply A * m (or m * A) by broadcasting m to a diagonal."""
+        if scipy.sparse.issparse(m):
+            if m.shape[0] == 1 or m.shape[1] == 1:
+                m = m.todense()
             else:
-                all_repos.append(f)
+                return A.multiply(m)
+        m = np.asarray(m)
+        if not 1 <= m.ndim <= 2:
+            raise ValueError("don't know how to multiply with ndim > 2")
+        rows = False
+        if m.ndim == 2:
+            if m.shape[0] == 1:
+                m = m[0]
+            elif m.shape[1] == 1:
+                m = m[:,0]
+                rows = True
+            else:
+                raise ValueError("m must be 1xN or Nx1")
+        n = len(m)
+        diag = scipy.sparse.dia_matrix((m[None,:], [0]), shape=(n,n))
+        if rows:
+            return diag * A
+        else:
+            return A * diag
 
-    other_users = count_sum( (u,1.0)
-            for r in all_repos
-            for u in r2u[r])
-    other_users.pop(user, None)
+    @classmethod
+    def normalize_rows(cls, A):
+        """Normalize each row of the sparse matrix A so each row sums to 1."""
+        inverse = 1 / A.sum(1)
+        inverse[np.isinf(inverse)] = 0
+        return cls.multiply_broadcast(A, inverse)
 
-    if cutoff is not None:
-        other_users = other_users.items()
-        other_users.sort(reverse=True, key=lambda x: x[1])
-        other_users = dict(other_users[:cutoff])
 
-    other_repos = count_sum( (r,v)
-            for u,v in other_users.iteritems()
-            for r in u2r[u] )
-    for r in repos:
-        other_repos.pop(r, None)
+    def prepare(self, forked_weight = 1.0):
+        nusers, nrepos = self.data.shape
 
-    results = other_repos.items()
-    results.sort(reverse=True, key=lambda x: x[1])
+        # r2r = # of users watching both i and j
+        D = self.data.tocsc()
+        r2r = D.transpose() * D
+        self.common_watchers = r2r.copy()
+        r2r.data **= 1.1
 
-    if n is None:
+        # Add links to parent repositories.  The weight of the parent
+        # repository is equal to the weight of itself times forked_weight.
+        weights = r2r.diagonal() * forked_weight
+        r2r = r2r + self.multiply_broadcast(self.forked, weights[:,None])
+
+        # r2r[i,j] = probability of going from repo i to repo j on each
+        # iteration
+        self.r2r = self.normalize_rows(r2r).tocsr()
+
+        # Normalize the data so we sum to 1.
+        self.u2r = self.normalize_rows(D).tocsr()
+
+        # Number of watchers of each repo.
+        self.watchers = np.asarray(self.data.sum(0))[0]
+        self.watchers /= self.watchers.sum()
+        self.most_popular = self.watchers.argsort()[::-1]
+        self.top_ten = self.most_popular.tolist()[:10]
+
+
+    def run(self, users, verbose = True, **kwargs):
+        results = {}
+        for u in users:
+            if verbose:
+                print(u)
+            results[u] = self.suggest(u, **kwargs)
+        self.results = results
         return results
-    else:
-        return [x[0] for x in results[:n]]
 
-def run_suggest(users, u2r, r2u, r2from, n=10, cutoff=100):
-    results = {}
-    for u in users:
-        print(u)
-        results[u] = suggest(u, u2r, r2u, r2from, n, cutoff)
-    return results
 
-def save_suggest(filename, results):
-    with open(filename, 'w') as f:
-        for k,v in sorted(results.iteritems()):
-            f.write('%s:%s\n' % (k, ','.join(map(str,v))))
+    def suggest(self, u, follow_factor = 0.25, top = 10, sorted=True):
+
+        watched = self.u2r[u]
+        #fork1 = watched * self.forked
+        #fork2 = fork1 * self.forked
+        #state = (watched + fork1 * .25 + fork2 * .25) * self.r2r
+        state = watched * self.r2r
+        #follows = self.followers[u] * self.u2r
+        #state = ((watched * self.r2r) * (1 - follow_factor)
+        #        + follows * follow_factor)
+        accumulator = state.todense()
+
+        # Sort the scores, ignoring those that are already being watched.
+        # Take the top results that are not already being watched.
+        #accumulator += accumulator.max() * self.watchers
+        watched = self.data[u].nonzero()
+        accumulator[watched] = 0
+        accumulator = np.asarray(accumulator)[0]
+        ranking = accumulator.argsort()[::-1]
+
+        # Make sure we are not outputting crap.
+        results = ranking[:top]
+        scores = accumulator[results]
+        l = np.sum(scores > 0)
+        answer = ranking[:l].tolist()
+
+        # If we have no other guess, default to the most popular.
+        # This only added two results; doesn't seem worth doing.
+        if l < top:
+            answer += self.top_ten[:top-l]
+
+        if sorted:
+            answer.sort()
+        return answer
+
+    def print_results(self, results=None, file=None):
+        if results is None:
+            results = self.results
+        if file is None:
+            file = sys.stdout
+        keys = results.keys()
+        keys.sort()
+        for k in keys:
+            print(k, ','.join(map(str,results[k])), sep=':', file=file)
+
+    def create_graph(self, user, filename, top=10, inner=False, forked=True):
+
+        with open(filename, 'w') as f:
+
+            f.write('digraph user_%s {\n' % user);
+            f.write(' node [shape=ellipse];\n')
+            f.write(' edge [dir=none];\n')
+
+            # Color all of the watched repositories.
+            repos = self.data[user].nonzero()[1]
+            for r in repos:
+                f.write(' "%s" [style=filled, fillcolor="#33cc33"];\n' % r)
+
+            w = self.common_watchers
+            done = set()
+
+            if forked:
+                for r in repos:
+                    x = self.forked[r]
+                    if x.nnz > 0:
+                        o = x.nonzero()[1][0]
+                        if (o,r) in done:
+                            continue
+                        done.add((r,o))
+                        f.write(' "%s" -> "%s" [label="%i", '
+                                'color=red, dir=forward];\n'
+                                % (r, o, w[r,o]))
+
+            if inner:
+                for a,b in itertools.combinations(repos, 2):
+                    if w[a,b] > 1:
+                        if (o,r) in done or (r,o) in done:
+                            continue
+                        done.add((r,o))
+                        f.write(' "%s" -> "%s" [label="%i"];\n'
+                                % (a, b, w[a,b]))
+
+            # Add all of the linked repositories.
+            if top > 0:
+                for r in repos:
+                    scores = np.asarray( w[r].todense(), dtype=int )[0]
+                    scores[r] = 0
+                    t = min(np.sum(scores > 1), top)
+                    others = scores.argsort()[:-t-1:-1]
+                    for o in others:
+                        if (o,r) in done or (r,o) in done:
+                            continue
+                        done.add((r,o))
+                        f.write(' "%s" -> "%s" [label="%s"];\n'
+                                % (r, o, scores[o]))
+
+            f.write('}\n')
+
+
+if __name__ == "__main__":
+    test = np.loadtxt('download/test.txt', dtype=int)
+    S = Suggestion()
+    S.prepare()
+    results = S.run(test)
+    S.print_results(results)
